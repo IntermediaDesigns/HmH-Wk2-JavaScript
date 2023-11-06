1707a5f81d590ad61c89b0edd4e2db3ea640d501
Lynjai Jimenez <139661024+IntermediaDesigns@users.noreply.github.com>
2023-11-06 15:32:51 -0500

    3

--- src/index.js
+++ src/index.js
@@ -1,27 +1,23 @@
 let aiPick = "rock";
 
-let humanPick;
-
 let humanPick = prompt(
-       "Attention, human challenger! Brace yourselves, for the AI has mastered the art of unwavering determination. Its choice will always be as solid as a rock! Prepare for an unyielding battle of wits in this game of Rock, Paper, Scissors! 💪🗿 #ChallengeAccepted Choose: rock, paper, or scissors!"
+       "Attention, human challenger! Brace yourselves, for the AI has mastered the art of unwavering determination. Its choice will always be as solid as a rock! Prepare for an unyielding battle of wits in this game of Rock, Paper, Scissors! 💪🗿 #ChallengeAccepted       Choose: rock, paper, or scissors!"
      );
      
      if (humanPick === "rock") {
        alert(
-              "Looks like we're both stuck between a rock and a hard place – the battle of wits continues!" 🪨🤖🤝🧔
+              "Looks like we're both stuck between a rock and a hard place – the battle of wits continues! 🪨🤖🤝🧔"
        );
      } 
      
      else if (humanPick === "paper") {
        alert(
-              "Well, I guess even the smartest AI can't resist the power of a well-folded sheet of paper! 🤖✋📄 #RockPaperScissorsAIFail"
-       );
+              "Well, I guess even the smartest AI can't resist the power of a well-folded sheet of paper! 🤖✋📄 #RockPaperScissorsAIFail");
      } 
      
      else if (humanPick === "scissors") {
        alert(
-              "Looks like I brought a 'rock' to a 'scissors' fight and still came out on top! I must have some 'rock-solid' luck today! 🤘✂️😄"
-       );
+              "Looks like I brought a 'rock' to a 'scissors' fight and still came out on top! I must have some 'rock-solid' luck today! 🤘✂️😄");
      } 
      
      
