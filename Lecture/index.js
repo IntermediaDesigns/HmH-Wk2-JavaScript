f7070c626aa69ae3c5d8dded9823e241bfc65637
Lynjai Jimenez <139661024+IntermediaDesigns@users.noreply.github.com>
2023-11-28 14:58:33 -0500

    1

---
+++ Lecture/index.js
@@ -0,0 +1,59 @@
+// we can send ourselves (the developer) messages from the running code
+
+// we can capture the input from prompt by using a variable
+
+// anytime we get input from prompt it's stored as a string
+
+//
+// console.log(typeof age);
+
+// booleans
+// can be true or false
+
+// operator !
+// switches true to false and vice versa
+
+// every value thats not a boolean has an built in property of either being truthy or falsy
+
+// every value is truthy - when converted to a boolean it will be equal to true
+// except these : null, undefined, nan, 0, "" - when converted to a boolean it will be equal to false
+
+// let or const
+// what is the difference between these two?
+// let can be changed, const - constant
+
+// how can we make decisions in our code
+// conditional statements (if)
+
+// if the age is 21 or over, do something
+
+// let age = Number(prompt("How old are you?"));
+
+// we need to convert the string to a number
+
+// age > 21 is what we call an expression. a piece of code that can be evaluated into a value
+
+// if (age >= 21) {
+//   alert("Come on in!");
+// } else {
+//   alert("get out!");
+// }
+
+// i only like to run if its above 60 degrees and the wind is less than 10 mph
+
+//asks me the temp
+// let temp = +prompt("What is the temp?");
+// // asks me the wind speed
+// let wind = +prompt("What is the wind speed in mph?");
+
+// if both are "good" tells me to run or stay home
+// logical operator (and operator) - if both values are true returns true and if one or both are false returns false
+// if (temp >= 60 && wind < 10) {
+//   alert("Go run!");
+// } else {
+//   alert("stay home!");
+// }
+
+// logical or operator , if either boolean is true returns true
+
+// console.log(true || false);
